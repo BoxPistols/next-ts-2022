8b79e44093b712444560a179c773c5adf75cb5dc
aito <BoxPistols@users.noreply.github.com>
2022-01-26 19:17:54 +0900

    ImageOptimization

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Script from "next/script";
+import Image from 'next/image'
 
 export default function FirstPost() {
   return (
@@ -21,6 +22,12 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      <Image
+        src="/img/vercel.svg"
+        alt="logo"
+        width={500/2}
+        height={500/2}
+      />
     </>
   );
 }
